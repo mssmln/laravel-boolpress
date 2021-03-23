ae8a9ce2b96bc9b1ddfea282c1c127e0311d42bb
Massimiliano <m.croci.ly@gmail.com>
2021-03-23 16:18:26 +0100

    added prefixes on web.php file

--- boolpress/routes/web.php
+++ boolpress/routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('homepage');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+//meglio usare i prefix su questa rotta 
+
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->group(function (){
+    Route::get('/','HomeController@index')->name('home');
+});
