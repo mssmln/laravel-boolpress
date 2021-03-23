3a05c3e668a9bf84d5eb55bf497c8d75b0c6626a
Massimiliano <m.croci.ly@gmail.com>
2021-03-23 17:35:17 +0100

    working on fakers

--- boolpress/app/Http/Controllers/Admin/HomeController.php
+++ boolpress/app/Http/Controllers/Admin/HomeController.php
@@ -1,4 +1,8 @@
 <?php
+/*Preparare le sezioni admin e gest del nostro progetto laravel-boolpress, così come visto a lezione.
+Migrate la tabella users e provate la registrazione ed il login.
+Create poi la taballa posts e i seeders.N.B. Se vi dovesse succedere il problema dell'out of memory potete provare con php artisan cache:clear,
+così svuota la memoria cache! Appuntiamo anche questo :sweat_smile:*/
 
 namespace App\Http\Controllers\Admin;
 
