c8003b66ae014d8450fd283afa554974cf6ece77
Massimiliano <m.croci.ly@gmail.com>
2021-03-23 17:53:08 +0100

    finished off

--- boolpress/database/seeds/PostSeeder.php
+++ boolpress/database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker; // model faker to add
+use App\Post; // model nostro to add so we can instance
+use Illuminate\Support\Str; // model slug to add
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +12,14 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i < 10; $i++){
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(2);
+            $newPost->content = $faker->text(500);
+            $newPost->slug = Str::slug($newPost->title); // usiamo lo slug per la prima volta , popolerà l'uri con un - tra le parole
+            $newPost->save();
+        }
     }
 }
